dfea86dd69676df81b37118cfd43f7fef3507295
auctionDeveloper <auctionwale.bank@gmail.com>
2025-05-13 13:11:52 +0530

    commit

--- myfrontend/vite.config.js
+++ myfrontend/vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: "/myfrontend/",
-
+  base: "/myfrontend/",  // Adjust this to the correct repository name
 })
