8241bf22e5e4b4ebfc48aed33fd7a7cc65a847f4
auctionDeveloper <auctionwale.bank@gmail.com>
2025-05-14 14:41:20 +0530

    commit

--- myfrontend/src/components/BankAuctionPage.jsx
+++ myfrontend/src/components/BankAuctionPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import INDIA from '../assets/mapindia.png';  // Path to your India map image
+import INDIA from '../assets/INDIA.svg';  // Path to your India map image
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import './BankAuctionPage.css';  // Import the CSS file
 
@@ -19,7 +19,7 @@ export default function BankAuctionPage() {
   };
 
   return (
-   <div className='flex flex-col'>
+   <div className='flex flex-col bg-[#7E7E7E]'>
     <div className='flex flex-col items-center justify-center my-5'>
       <h1 className='font-extrabold text-2xl'>Bank Auction</h1>
       <p>Explore</p>
@@ -34,7 +34,7 @@ export default function BankAuctionPage() {
 
       {/* Jammu & Kashmir - Button with CSS class for positioning */}
       <button 
-        className="state jammu font-bold"
+        className="state jammu font-semibold"
         onClick={() => handleStateClick('Jammu & Kashmir')}
       >
         <FiberManualRecordIcon />
@@ -43,11 +43,46 @@ export default function BankAuctionPage() {
 
       {/* Punjab - Button with CSS class for positioning */}
       <button 
-        className="state punjab font-extrabold"
+        className="state punjab font-semibold"
         onClick={() => handleStateClick('Punjab')}
       >
         <FiberManualRecordIcon />
         Punjab
+      </button>
+      
+
+            {/* Himachal Pardesh - Button with CSS class for positioning */}
+      <button 
+        className="state himachal font-semibold"
+        onClick={() => handleStateClick('Himachal')}
+      >
+        <FiberManualRecordIcon />
+        Himachal Pradesh
+      </button>
+
+            {/* Uttrakhand - Button with CSS class for positioning */}
+      <button 
+        className="state uttrakhand font-semibold"
+        onClick={() => handleStateClick('Uttrakhand')}
+      >
+        <FiberManualRecordIcon />
+      Uttrakhand
+      </button>
+            {/* Haryana - Button with CSS class for positioning */}
+      <button 
+        className="state haryana font-semibold"
+        onClick={() => handleStateClick('Haryana')}
+      >
+        <FiberManualRecordIcon />
+     Harayana
+      </button>
+            {/* Delhi - Button with CSS class for positioning */}
+      <button 
+        className="state delhi font-semibold"
+        onClick={() => handleStateClick('Delhi')}
+      >
+        <FiberManualRecordIcon />
+     Delhi
       </button>
 
       {/* Add more states here */}
